c1e83cea9b9bad6bec47b39a67702053414cbdf2
Danila Shutov <dcbrwn2@gmail.com>
2017-08-19 19:06:16 +0400

    Refactor main POC page

--- src/photospice.jsx
+++ src/photospice.jsx
@@ -5,30 +5,40 @@ import mix from './fx/mix';
 import SceneGraph from './lib/renderer/SceneGraph';
 import Sampler2DNode from './lib/renderer/Sampler2DNode';
 import ProgramNode from './lib/renderer/ProgramNode';
-import TargetNode from './lib/renderer/TargetNode';
+import ScreenNode from './lib/renderer/ScreenNode';
+
+import GraphView from './components/GraphView';
+// import NodeView from './components/NodeView';
+
+const scene = new SceneGraph();
+const spice = new Sampler2DNode();
+const testImage = new Sampler2DNode();
+const mixer = new ProgramNode(mix);
+const screen = new ScreenNode();
+scene.attachNode(spice);
+scene.attachNode(testImage);
+scene.attachNode(mixer);
+scene.attachNode(screen);
+scene.connect(spice, 'texture', mixer, 'image1');
+scene.connect(testImage, 'texture', mixer, 'image2');
+scene.connect(mixer, 'result', screen, 'uImage');
 
 class Page extends React.Component {
   async componentDidMount() {
-    const scene = new SceneGraph();
-    const spice = new Sampler2DNode();
-    const testImage = new Sampler2DNode();
-    const mixer = new ProgramNode(mix);
-    const target = new TargetNode(this.target);
+    screen.setTarget(this.target);
     await spice.loadFromUrl('./assets/spice.jpg');
     await testImage.loadFromUrl('./assets/jelly-beans.png');
-    scene.attachNode(spice);
-    scene.attachNode(testImage);
-    scene.attachNode(mixer);
-    scene.attachNode(target);
-    scene.connect(spice, 'texture', mixer, 'image1');
-    scene.connect(testImage, 'texture', mixer, 'image2');
-    scene.connect(mixer, 'result', target, 'uImage');
-    scene.run(target);
+    scene.run(screen);
   }
 
   render() {
     return (
-      <div>
+      <div
+        style={{
+          width: '100%',
+          height: '100vh',
+        }}>
+        <GraphView graph={scene} />
         <canvas ref={(target) => this.target = target} />
       </div>
     );
