7fb3b212c1b6500f4c2ecfd9ea37bcba79649186
Danila Shutov <dcbrwn2@gmail.com>
2017-08-20 22:14:00 +0400

    Add a little benchmark to main page

--- src/photospice.jsx
+++ src/photospice.jsx
@@ -6,11 +6,13 @@ import SceneGraph from './lib/renderer/SceneGraph';
 import Sampler2DNode from './lib/renderer/Sampler2DNode';
 import ProgramNode from './lib/renderer/ProgramNode';
 import ScreenNode from './lib/renderer/ScreenNode';
+import SceneNode from './lib/renderer/SceneNode';
 
+import ImageContainer from './components/ImageContainer';
 import GraphView from './components/GraphView';
-// import NodeView from './components/NodeView';
 
 const scene = new SceneGraph();
+// benchmark(100);
 const spice = new Sampler2DNode();
 const testImage = new Sampler2DNode();
 const mixer = new ProgramNode(mix);
@@ -33,15 +35,57 @@ class Page extends React.Component {
 
   render() {
     return (
-      <div
-        style={{
-          width: '100%',
-          height: '100vh',
-        }}>
+      <div className='l-photospice'>
+        {/* <ImageContainer toggleCompactMode={this.toggleCompactMode}>
+        </ImageContainer> */}
+        <canvas style={{display: 'none'}} ref={(target) => this.target = target} />
         <GraphView graph={scene} />
-        <canvas ref={(target) => this.target = target} />
       </div>
     );
+  }
+}
+
+function benchmark(count = 100) {
+  class ValueNode extends SceneNode {
+    constructor(value) {
+      super({
+        outputs: {
+          out: { type: 'float', name: 'Value' },
+        },
+      });
+      this.value = value;
+    }
+
+    run() {
+      return { out: this.value };
+    }
+  }
+
+  class NegateNode extends SceneNode {
+    constructor() {
+      super({
+        inputs: {
+          in: { type: 'float', name: 'Input' },
+        },
+        outputs: {
+          out: { type: 'float', name: 'Negated' },
+        },
+      });
+    }
+
+    run(inputs) {
+      return -inputs.in;
+    }
+  }
+
+  let prev = new ValueNode(10);
+  scene.attachNode(prev);
+
+  for (let i = 0; i < count; i += 1) {
+    const negate = new NegateNode();
+    scene.attachNode(negate);
+    scene.connect(prev, 'out', negate, 'in');
+    prev = negate;
   }
 }
 
