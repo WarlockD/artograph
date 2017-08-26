be591dffb39f47444574763f4846a1352f552042
Danila Shutov <dcbrwn2@gmail.com>
2017-08-26 21:35:58 +0400

    Add audio noise node

---
+++ src/lib/renderer/nodes/AudioNoiseNode.js
@@ -0,0 +1,61 @@
+import SceneNode from '../SceneNode';
+import { audio } from '../screen';
+
+export default class AudioNoiseNode extends SceneNode {
+  static nodeName = 'Noise';
+  constructor() {
+    super({
+      name: AudioNoiseNode.nodeName,
+      inputs: {
+        gain: {
+          name: 'Gain',
+          type: 'float',
+          value: 0.5,
+        },
+      },
+      outputs: {
+        output: {
+          type: 'sound',
+          name: 'Output',
+        },
+      }
+    });
+
+    const bufferSize = 2 * audio.sampleRate;
+    const noiseBuffer = audio.createBuffer(1, bufferSize, audio.sampleRate);
+    const output = noiseBuffer.getChannelData(0);
+
+    for (var i = 0; i < bufferSize; i++) {
+      output[i] = Math.random() * 2 - 1;
+    }
+
+    this.whiteNoise = audio.createBufferSource();
+    this.whiteNoise.buffer = noiseBuffer;
+    this.whiteNoise.loop = true;
+    this.whiteNoise.start(0);
+
+    this.audioNode = audio.createGain();
+    this.whiteNoise.connect(this.audioNode);
+  }
+
+  onBeforeConnect(sourceNode, sourcePin, targetNode, targetPin) {
+    if (sourceNode === this) {
+      this.audioNode.connect(targetNode.audioNode);
+    }
+  }
+
+  onBeforeDisconnect(sourceNode, sourcePin, targetNode, targetPin) {
+    if (sourceNode === this) {
+      this.audioNode.disconnect(targetNode.audioNode);
+    }
+  }
+
+  run(inputs) {
+    try {
+      this.audioNode.gain.value = inputs.gain;
+    } catch (e) {
+      console.error(e);
+    }
+    return { output: this.audioNode };
+  }
+}
