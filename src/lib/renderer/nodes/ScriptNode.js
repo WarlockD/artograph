620a602858e906d857903d2a5bc799a501ba2ccb
Danila Shutov <dcbrwn2@gmail.com>
2017-08-26 21:07:04 +0400

    Rework script node schema

--- src/lib/renderer/nodes/ScriptNode.js
+++ src/lib/renderer/nodes/ScriptNode.js
@@ -7,26 +7,47 @@ export default class ScriptNode extends SceneNode {
     super({
       name: 'Script',
       inputs: {
-        input: {
-          name: 'Input',
+        input: {name: 'Input',
           type: 'float',
           value: 0.5,
         },
       },
       outputs: {
-        output: {
-          type: 'float',
-          name: 'Output',
-        },
+        a: { type: 'float', name: 'a' },
+        b: { type: 'float', name: 'b' },
+        c: { type: 'float', name: 'c' },
+        d: { type: 'float', name: 'd' },
       }
     });
 
-    this.compile('return 0;')
+    this.compile('')
   }
 
   compile(code) {
+    'use strict';
+
+    const allowedGlobals = ['Math', 'Date'];
+    const globals = Object.keys(window).reduce((acc, global) => {
+      const overrideNeeded = allowedGlobals.indexOf(global) === -1 && /^\w+$/.test(global);
+      if (overrideNeeded) {
+        acc += `let ${global};\n`;
+      }
+      return acc;
+    }, '');
+    const script = `
+      'use strict';
+
+      ${globals}
+
+      (function(input) {
+        let a = 0, b = 0, c = 0, d = 0;
+        ${code}
+        return { a: a, b: b, c: c, d: d };
+      })
+    `;
+
     try {
-      this.func = new Function('input', code);
+      this.func = eval(script);
       this.code = code;
     } catch (e) {
       console.error(e);
@@ -35,7 +56,7 @@ export default class ScriptNode extends SceneNode {
 
   run(inputs) {
     try {
-      return { output: this.func(inputs.input) };
+      return this.func(inputs.input);
     } catch (e) {
       console.error(e);
     }
