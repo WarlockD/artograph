5281e51629d9f47e8b8a2a54bd9b877cdef85a19
Danila Shutov <dcbrwn2@gmail.com>
2017-08-28 18:32:29 +0400

    Implement node factory to unify nodes construction

---
+++ src/lib/renderer/NodeFactory.js
@@ -0,0 +1,22 @@
+import { assert } from '../utils';
+import nodes from './nodes';
+import { ScreenNode } from './ScreenNode';
+
+class NodeFactory {
+  constructor() {
+    this.mapping = nodes.reduce((acc, Node) => {
+      acc[Node.name] = () => new Node();
+      return acc;
+    }, {});
+
+    // Since screen node as a singleton it requires special handling
+    this.mapping['ScreenNode'] = () => ScreenNode;
+  }
+
+  createNode(type) {
+    assert(!(type in this.mapping), 'Unknown node type');
+    return this.mapping[type]();
+  }
+}
+
+export default new NodeFactory();
