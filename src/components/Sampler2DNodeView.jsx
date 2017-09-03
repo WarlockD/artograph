7b9aaff9029fb45ba9d7701a1bd8abb80cd25aeb
Danila Shutov <dcbrwn2@gmail.com>
2017-09-03 21:49:35 +0400

    Use nice preview for sampler nodes

--- src/components/Sampler2DNodeView.jsx
+++ src/components/Sampler2DNodeView.jsx
@@ -6,30 +6,48 @@ import NodeView from './NodeView';
 export default class Sampler2DNodeView extends NodeView {
   static modelNode = Sampler2DNode;
 
+  constructor(props) {
+    super(props);
+
+    this.state.previewUrl = props.node.image
+      ? props.node.image.src
+      : '/assets/placeholder.svg';
+  }
+
   @bound
   uploadImage(event) {
     const reader = new FileReader();
     reader.onload = async (event) => {
       const url = event.target.result;
       const node = this.props.node;
-      node.loadFromUrl(url);
+      await node.loadFromUrl(url);
+      this.setState({ previewUrl: url });
     };
     reader.readAsDataURL(event.target.files[0]);
   }
 
   renderContents() {
+    const previewStyle = {
+      backgroundColor: 'black',
+      backgroundImage: `url(${this.state.previewUrl})`,
+      backgroundSize: 'contain',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+      width: 128,
+      height: 128,
+    };
+
     return <div>
+      <div
+        style={previewStyle}
+        title='Click to upload an image'
+        onClick={() => this.input.click()}/>
       <input
         type='file'
         accept='image/*'
         style={{ display: 'none' }}
         ref={(input) => this.input = input}
         onChange={this.uploadImage}/>
-      <button
-        className='button'
-        onClick={() => this.input.click()}>
-        Upload
-      </button>
     </div>;
   }
 }
